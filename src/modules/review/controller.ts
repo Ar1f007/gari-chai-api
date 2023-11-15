a6a91f2bef43ce0bab169c7c1c2fc9383ef2361e
Ariful Hoque Al Amin <muhammadarif9416@yahoo.com>
2023-11-15 07:05:12 +0600

    add: review model and schema with service

---
+++ src/modules/review/controller.ts
@@ -0,0 +1,36 @@
+import { Request, Response } from 'express';
+import {
+  createNewReview,
+  //  deleteReview, findAndUpdateReview, findReview,
+  findReviews,
+} from './service';
+import {
+  CreateNewReviewInputs,
+  ReadReviewsByCarInput,
+  // DeleteReviewInput, ReadReviewInput, UpdateReviewInput
+} from './schema';
+import { StatusCodes } from 'http-status-codes';
+import AppError from '../../utils/appError';
+
+export async function createReviewHandler(req: Request<{}, {}, CreateNewReviewInputs>, res: Response) {
+  const review = await createNewReview(req.body);
+
+  if (!review) {
+    throw new AppError('Could not create review', StatusCodes.BAD_REQUEST);
+  }
+
+  res.status(StatusCodes.CREATED).json({
+    status: 'success',
+    data: review,
+    message: 'Review is added successfully',
+  });
+}
+
+export async function getReviewsHandler(req: Request<ReadReviewsByCarInput>, res: Response) {
+  const reviews = await findReviews({ carId: req.params.carId });
+
+  res.status(StatusCodes.OK).json({
+    status: 'success',
+    data: reviews,
+  });
+}
