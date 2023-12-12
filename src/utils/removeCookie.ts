abb05bd69364781d1fe19a76f2a92a24ad255561
Ariful Hoque Al Amin <muhammadarif9416@yahoo.com>
2023-12-13 00:19:41 +0600

    add: clear cookie

--- src/utils/removeCookie.ts
+++ src/utils/removeCookie.ts
@@ -1,5 +1,10 @@
 import { Response } from 'express';
+import { envVariables } from './env';
+import { API_DOMAIN } from '../constants';
 
 export const removeCookie = (cookieName: string, res: Response) => {
-  res.clearCookie(cookieName);
+  res.clearCookie(cookieName, {
+    domain: envVariables.NODE_ENV === 'development' ? 'localhost' : API_DOMAIN,
+    path: '/',
+  });
 };
