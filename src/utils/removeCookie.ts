83c77d4877c586cf1ee6ec538b98d1dd3fa68b73
Ariful Hoque Al Amin <muhammadarif9416@yahoo.com>
2023-12-13 00:54:22 +0600

    update: set value to empty string for cookie

--- src/utils/removeCookie.ts
+++ src/utils/removeCookie.ts
@@ -4,7 +4,7 @@ import { envVariables } from './env';
 export const removeCookie = (cookieName: string, res: Response) => {
   const oneDay = 24 * 60 * 60 * 1000;
 
-  res.cookie(cookieName, {
+  res.cookie(cookieName, '', {
     path: '/',
     expires: new Date(Date.now() - oneDay),
     httpOnly: true,
