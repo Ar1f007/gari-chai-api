43c2deb40de2850d7d657dabea655062f00e8903
Ariful Hoque Al Amin <muhammadarif9416@yahoo.com>
2023-12-13 00:49:38 +0600

    remove cookie

--- src/utils/removeCookie.ts
+++ src/utils/removeCookie.ts
@@ -1,10 +1,14 @@
 import { Response } from 'express';
+import { envVariables } from './env';
 
 export const removeCookie = (cookieName: string, res: Response) => {
   const oneDay = 24 * 60 * 60 * 1000;
 
-  res.clearCookie(cookieName, {
+  res.cookie(cookieName, {
     path: '/',
     expires: new Date(Date.now() - oneDay),
+    httpOnly: true,
+    secure: envVariables.NODE_ENV === 'production',
+    sameSite: envVariables.NODE_ENV === 'production' ? 'none' : 'lax',
   });
 };
