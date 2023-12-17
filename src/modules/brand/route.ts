e0fd89a163425c1a0a1bc8b01767db66109f1500
Ariful Hoque Al Amin <muhammadarif9416@yahoo.com>
2023-12-18 00:42:38 +0600

    add: delete car brand

--- src/modules/brand/route.ts
+++ src/modules/brand/route.ts
@@ -9,18 +9,21 @@ import {
   updateBrandHandler,
 } from './controller';
 
-import { createBrandSchema, deleteBrandSchema, getBrandSchema, updateBrandSchema } from './schema';
+import { createBrandSchema, getBrandSchema, updateBrandSchema } from './schema';
 
 import { validateResource } from '../../middleware';
 
 export const brandRouter = express.Router();
 
-brandRouter.route('/').get(getBrandsHandler).post(validateResource(createBrandSchema), createBrandHandler);
+brandRouter
+  .route('/')
+  .get(getBrandsHandler)
+  .post(validateResource(createBrandSchema), createBrandHandler)
+  .delete(deleteBrandHandler);
 
 brandRouter.get('/all-with-popular', getAllAndPopularBrandsHandler); // used in search box
 
 brandRouter
   .route('/:brandSlug')
   .get(validateResource(getBrandSchema), getBrandHandler)
-  .put(validateResource(updateBrandSchema), updateBrandHandler)
-  .delete(validateResource(deleteBrandSchema), deleteBrandHandler);
+  .put(validateResource(updateBrandSchema), updateBrandHandler);
